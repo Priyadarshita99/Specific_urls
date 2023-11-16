0b0cf4eb21538ceec4eaabfbea08fa2d81367856
Priya <gsprout74@gmail.com>
2023-11-16 13:05:01 +0530

    first commit

---
+++ kkr/urls.py
@@ -0,0 +1,8 @@
+from kkr.views import *
+from django.urls import path
+
+app_name='something'
+urlpatterns=[
+path('sunil/',sunil,name='sunil'),
+
+]
---
+++ kkr/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sunil(request):
+    return render(request,'sunil.html')
